f6f5f275d6672f7404cdb3f1400e516d93932103
barotraumaenjoyer <npuig.dam@institutcampalans.net>
2025-12-17 17:31:36 +0100

    wpf amb client

--- Kanban/Kanban/Controllers/UserController.cs
+++ Kanban/Kanban/Controllers/UserController.cs
@@ -1,13 +1,198 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
+using Kanban.Model;
+
 
 namespace Kanban.Controllers
 {
-    internal class UserController 
+    public class UserController
     {
+        string BaseUri;
+
+
+        public UserController()
+        {
+            BaseUri = ConfigurationManager.AppSettings["BaseUri"];
+        }
+
+        public async Task<List<User>> GetAllUsersAsync()
+        {
+            List<User> users = new List<User>();
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(BaseUri);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(
+                    new MediaTypeWithQualityHeaderValue("application/json")
+                );
+
+                // GET /user
+                HttpResponseMessage response = await client.GetAsync("user");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    if (response.StatusCode == System.Net.HttpStatusCode.NoContent)
+                    {
+                        users = new List<User>();
+                    }
+                    else
+                    {
+                        users = await response.Content.ReadAsAsync<List<User>>();
+                    }
+                }
+                else
+                {
+                    // TODO: error handling
+                }
+            }
+
+            return users;
+        }
+
+        public async Task<int> GetUserTaskCountAsync(int id)
+        {
+            int count = 0;
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(BaseUri);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(
+                    new MediaTypeWithQualityHeaderValue("application/json")
+                );
+
+                // GET /user/count/{id}
+                HttpResponseMessage response = await client.GetAsync($"user/count/{id}");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    if (response.StatusCode != System.Net.HttpStatusCode.NoContent)
+                    {
+                        string json = await response.Content.ReadAsStringAsync();
+                        count = int.Parse(json);
+
+                    }
+                }
+                else if (response.StatusCode == System.Net.HttpStatusCode.NotFound)
+                {
+                    count = 0;
+                }
+                else
+                {
+                    // TODO: handle other errors (log / throw)
+                }
+            }
+
+            return count;
+        }
+
+        public async Task<User> InsertUserAsync(User user)
+        {
+            User createdUser = null;
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(BaseUri);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(
+                    new MediaTypeWithQualityHeaderValue("application/json")
+                );
+
+                // POST /user
+                HttpResponseMessage response = await client.PostAsJsonAsync("user", user);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    if (response.StatusCode != System.Net.HttpStatusCode.NoContent)
+                    {
+                        createdUser = await response.Content.ReadAsAsync<User>();
+                    }
+                }
+                else
+                {
+                    // TODO: handle error (log / throw)
+                }
+            }
+
+            return createdUser;
+        }
+
+        public async Task<bool> UpdateUserAsync(int id, User user)
+        {
+            bool updated = false;
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(BaseUri);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(
+                    new MediaTypeWithQualityHeaderValue("application/json")
+                );
+
+                // PUT /user/{id}
+                HttpResponseMessage response = await client.PutAsJsonAsync($"user/{id}", user);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    // 204 → update successful
+                    if (response.StatusCode == System.Net.HttpStatusCode.NoContent)
+                    {
+                        updated = true;
+                    }
+                }
+                else if (response.StatusCode == System.Net.HttpStatusCode.NotFound)
+                {
+                    updated = false;
+                }
+                else
+                {
+                    // TODO: handle other errors (log / throw)
+                }
+            }
+
+            return updated;
+        }
+
+
+        public async Task<bool> DeleteUserAsync(int id)
+        {
+            bool deleted = false;
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(BaseUri);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(
+                    new MediaTypeWithQualityHeaderValue("application/json")
+                );
+
+                // DELETE /user/{id}
+                HttpResponseMessage response = await client.DeleteAsync($"user/{id}");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    // 200 or 204 → deleted successfully
+                    deleted = true;
+                }
+                else if (response.StatusCode == System.Net.HttpStatusCode.NotFound)
+                {
+                    deleted = false;
+                }
+                else
+                {
+                    // TODO: handle other errors (log / throw)
+                }
+            }
+
+            return deleted;
+        }
 
     }
 }
---
+++ Kanban/Kanban/Model/Tasques.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kanban.Model
+{
+    public class Tasques
+    {
+        public int Id { get; set; }
+        public string Nom { get; set; }
+        public string Descripcio { get; set; }
+        public int? UsuariId { get; set; }
+        public int? Prioritat { get; set; }
+        public int? Estat { get; set; }
+        public DateTime? DataFinal { get; set; }
+    }
+}
